88c1c5098542c0414a345e97e181728ce00d3bea
Prajjwal Srivastav <prajjwal@Prajjwal-Macbook-Pro.local>
2023-12-20 13:55:02 +0530

    added filter

---
+++ filter.py
@@ -0,0 +1,8 @@
+number_list = range(-7,15)
+
+greater_than_zero = list(filter(lambda x: x > 0, number_list))  # filter out all numbers less than zero
+print(greater_than_zero)
+
+#list of zero
+zeroes = [0]*5
+print(zeroes)
